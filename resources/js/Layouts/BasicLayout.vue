a06c780459cd09eaca963826892e1a1477e05c3f
Mellester <mellester1@gmail.com>
2021-01-07 16:01:59 +0100

    croos

--- resources/js/Layouts/BasicLayout.vue
+++ resources/js/Layouts/BasicLayout.vue
@@ -1,9 +1,6 @@
 <template>
   <main>
     <header>
-      <inertia-link href="/">Home</inertia-link>
-      <inertia-link  href="/login" :active="route().current('login')">About</inertia-link>
-      <inertia-link href="/register">Contact</inertia-link>
     </header>
     <article>
       <slot />
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -2,25 +2,29 @@
 <body>
     <div class="site">
     <header class="topbar header">
-        <jet-nav-link href="/" :active="true">
+        <a href="/" :active="true">
             <span class="sr-only">home page</span>
             {{ $inertia.page.props.appName }}
-        </jet-nav-link>
+        </a>
         <button @click="isOpen = !isOpen" id="menuBttn"
          class="items-center block text-gray-500 hover:text-white">
-               <svg viewBox="0 0 100 80" width="30" height="30">
-                <rect width="100" height="20"></rect>
-                <rect y="30" width="100" height="20"></rect>
-                <rect y="60" width="100" height="20"></rect>
+            <svg v-if="!isOpen" viewBox="0 0 100 80" width="30" height="30">
+                <rect width="100" height="20" rx="15"></rect>
+                <rect y="30" width="100" height="20" rx="15"></rect>
+                <rect y="60" width="100" height="20" rx="15"></rect>
             </svg>
+            <svg v-else viewBox="0 0 20 20" width="30" height="30">
+			<path fill="black" d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
+            </svg>
+            
         </button>
         <nav :class="isOpen  ? 'block' : 'hidden'" class="nav flex items-begin justify-end">
-        <jet-responsive-nav-link class="nav route" :href="route('register')" :active="route().current('register')">
+        <a class="nav route" :href="route('register')" :active="route().current('register')">
                             Register
-        </jet-responsive-nav-link>
-        <jet-responsive-nav-link as="button" class="nav route" :href="route('login')" :active="route().current('login')">
+        </a>
+        <a as="button" class="nav route" :href="route('login')" :active="route().current('login')">
                             Sign in
-        </jet-responsive-nav-link>
+        </a>
         </nav>
     </header>
       
@@ -33,12 +37,6 @@
 
 <script>
     import Welcome from '@/Jetstream/Welcome'
-import AppLayout from '../Layouts/AppLayout.vue'
-    import JetApplicationMark from '@/Jetstream/ApplicationMark'
-    import JetDropdown from '@/Jetstream/Dropdown'
-    import JetDropdownLink from '@/Jetstream/DropdownLink'
-    import JetNavLink from '@/Jetstream/NavLink'
-    import JetResponsiveNavLink from '@/Jetstream/ResponsiveNavLink'
 
      import BasicLayout from '../Layouts/BasicLayout.vue'
 
@@ -48,14 +46,10 @@ import AppLayout from '../Layouts/AppLayout.vue'
                 isOpen: false,
             }
         },
-        layout: (h, page) => h(BasicLayout, [page]),
-         layout: BasicLayout,
+        //layout: (h, page) => h(BasicLayout, [page]),
+        // layout: BasicLayout,
         components: {
-            AppLayout,
-            Welcome,
-            JetResponsiveNavLink,
-            JetNavLink,
-            JetDropdownLink,
+
         },
     }
 </script>
